f0b0b55ce535c4e23863e0abaf341858f91135ff
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-03-29 11:49:53 -0700

    [ package/log ]: install winston package and configure log to use(not yet used)!

--- server/routers/home.js
+++ server/routers/home.js
@@ -1,7 +1,8 @@
 const { Router } = require("express")
 const app = Router()
 const homeController = require("../controllers/homeController")
-
+// import logger from '../logs/logger'
+// logger.info("Hello visiter!")
 app.get("/", homeController)
 
 module.exports = app
