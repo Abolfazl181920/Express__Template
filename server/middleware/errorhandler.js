317f29b3051d3c368978abab7e5162b0979ebf13
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-03-30 03:01:59 -0700

    [ log ]: use log winston in err middleware!

--- server/middleware/errorhandler.js
+++ server/middleware/errorhandler.js
@@ -1,7 +1,13 @@
+const { logger } = require("../logs/logger")
+
 const errorHandler = (err, res, next) => {
-    if (res.headersSent) return next(err)
-    res.status(500)
-    res.render('error', { error: err })
+    if (res.headersSent) {
+        return next(err)
+    } else {
+        res.status(500)
+        res.render('error', { error: err })
+        logger.info(`${req.method} ${req.url}`)
+    }
 }
 
 module.exports = { errorHandler }
